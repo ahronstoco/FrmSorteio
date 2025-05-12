9f5248772f01e0f86aa75c51c28c337161c99010
Ahron <Ahron@Ahron>
2025-05-12 19:14:36 -0300

    Exemplo de Frame de um Sorteio.

---
+++ src/main/java/principal/Principal.java
@@ -0,0 +1,13 @@
+package principal;
+
+import visao.FrmSorteio;
+
+public class Principal {
+     
+    public static void main(String[] args) {
+        // Cria objeto do JFrame FrmSorteio
+        FrmSorteio objetotela = new FrmSorteio();
+        // Abre a FrmSorteio
+        objetotela.setVisible(true);
+    }
+ }
